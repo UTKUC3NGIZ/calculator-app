36df6eda3dabd4ba2a58222d3ca435297fc10520
Utku Cengiz <cekicengiz01@gmail.com>
2023-05-01 19:26:17 +0300

    theme section 1440px completed

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,7 +1,21 @@
+import "./style.css";
 function Theme() {
   return (
     <>
-      <h1>calc</h1>
+      <section className="themeMain">
+        <div>
+          <h1>calc</h1>
+        </div>
+        <div>
+          <h3>THEME</h3>
+          <span className="bar">
+           <span className="bar__theme">1 2 3</span>
+           <span className="bar__themeAnimation">
+            <span></span>
+           </span>
+          </span>
+        </div>
+      </section>
     </>
   );
 }
