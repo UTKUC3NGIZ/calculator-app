34f2bac81a8a8295c5beb3507f75eefa2f81d215
Utku Cengiz <cekicengiz01@gmail.com>
2023-05-05 17:06:39 +0300

    key functions assigned

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,37 +1,42 @@
+import { useState } from "react";
 import "./style.css";
 
 function Buttons() {
+  const [input, setInput] = useState([]);
+  console.log(input);
+
+
   return (
     <>
       <div className="mainButtons">
         <div>
           <div>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button className="functionButtonBlue">DEL</button>
+            <button onClick={() => setInput([...input, "7"])}>7</button>
+            <button onClick={() => setInput([...input, "8"])}>8</button>
+            <button onClick={() => setInput([...input, "7"])} >9</button>
+            <button className="functionButtonBlue" onClick={() => setInput([...input, "del"])} >DEL</button>
           </div>
           <div>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
+            <button onClick={() => setInput([...input, "4"])} >4</button>
+            <button onClick={() => setInput([...input, "5"])} >5</button>
+            <button onClick={() => setInput([...input, "6"])} >6</button>
+            <button onClick={() => setInput([...input, "+"])} >+</button>
           </div>
           <div>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>-</button>
+            <button onClick={() => setInput([...input, "1"])} >1</button>
+            <button onClick={() => setInput([...input, "2"])} >2</button>
+            <button onClick={() => setInput([...input, "3"])} >3</button>
+            <button onClick={() => setInput([...input, "-"])} >-</button>
           </div>
           <div>
-            <button>.</button>
-            <button>0</button>
-            <button>/</button>
-            <button>x</button>
+            <button onClick={() => setInput([...input, "."])} >.</button>
+            <button onClick={() => setInput([...input, "0"])} >0</button>
+            <button onClick={() => setInput([...input, "/"])} >/</button>
+            <button onClick={() => setInput([...input, "x"])} >x</button>
           </div>
           <div>
-            <button className="functionButtonBlue">RESET</button>
-            <button className="functionButtonRed">=</button>
+            <button className="functionButtonBlue" onClick={() => setInput([...input, "reset"])}>RESET</button>
+            <button className="functionButtonRed" onClick={() => setInput([...input, "="])}>=</button>
           </div>
         </div>
       </div>
